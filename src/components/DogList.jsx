240f246a258b22e2b15ec7fcdf3ff25124622492
tomekgora <tomekgora@gmail.com>
2019-11-21 13:27:06 +0100

    made DogList.jsx

---
+++ src/components/DogList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class DogsList extends Component {
+    render() {
+        return (
+            <div className='dogs-list'>
+                <h1>Dogs List</h1>
+            </div>
+        );
+    }
+}
